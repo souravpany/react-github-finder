7d95739368d6000ac4658db897d7083d4fbb5144
sourav.kumar.pany <sourav.kumar.pany@gds.ey.com>
2022-11-15 16:50:05 +0530

    1.Add API details(Base URL and Github token) 2.Add User search functionality with API 3.Add Alert show/hide functionality 4.Add Routing functionality 5.Add UI Header, Footer and Main content

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import NotFound from './pages/NotFound';
+import About from './pages/About';
+import Home from './pages/Home';
+import Alert from './components/layout/Alert';
+import {GithubProvider} from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
+
+function App() {
+  return (
+    <GithubProvider>
+        <AlertProvider>
+          <Router>
+            <div className="flex flex-col justify-between h-screen">
+              <Navbar/>
+                <main className='container mx-auto px-3 pb-12'>
+                  <Routes>
+                  <Route
+                    path='/'
+                    element={
+                            <>
+                              <Alert />
+                              <Home />
+                            </>
+                          }
+                  />
+                <Route path='/about' element={<About />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='*' element={<NotFound />} />
+                </Routes>
+              </main>
+              <Footer/>
+            </div>
+          </Router>
+       </AlertProvider>
+    </GithubProvider>
+  );
+}
+
+export default App;
